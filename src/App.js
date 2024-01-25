db8ba8c5127c9a4f5adaf35d4ab80d8537a434f1
sethm3141 <sethmaupin@gmail.com>
2024-01-24 23:07:41 -0600

    worked on the beginning of the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 function App() {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <section>
+        <h2>25 + 5 Clock</h2>
+      </section>
+      <section>
+        <div>
+          <h3 id='break-label'>Break Length</h3>
+          <div>
+            <i className='fa-solid fa-circle-arrow-up'></i>
+            <i className='fa-solid fa-circle-arrow-down'></i>
+          </div>
+        </div>
+        <div>
+          <div id='session-label'>Session Length</div>
+          <div></div>
+        </div>
+      </section>
+      <section>howdy</section>
+      <section>hey</section>
+    </>
+  );
 }
 
 export default App;
